2b87d1de74f42e5464c398460db4900908abf58b
vinay acharya <htvinaykumar98@gmail.com>
2020-05-08 20:49:05 +0530

    Updates

---
+++ precache-manifest.f9c2a2577c387193964330bc16bae26b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f41a11abba348392e0dad21d0c5b9c99",
+    "url": "/vinayportfolio/index.html"
+  },
+  {
+    "revision": "ac007ff42f9d9844c726",
+    "url": "/vinayportfolio/static/css/main.c8585616.chunk.css"
+  },
+  {
+    "revision": "cad618e39c3029cf839e",
+    "url": "/vinayportfolio/static/js/2.e0ba28e0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/vinayportfolio/static/js/2.e0ba28e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac007ff42f9d9844c726",
+    "url": "/vinayportfolio/static/js/main.8cdbffda.chunk.js"
+  },
+  {
+    "revision": "4f903b5a81fdacdfadbf",
+    "url": "/vinayportfolio/static/js/runtime-main.742e4c99.js"
+  }
+]);
